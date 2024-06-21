37ceefc5149740e248b751db5794415609f676f0
Jennifer Cao <jennifer.cao.ca@gmail.com>
2024-06-21 16:53:50 -0400

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Homepage/Pages/Navbar';
 import Home from './Homepage/Home';
 import HomeBody from './Homepage/HomeBody';
 import HomeFooter from './Homepage/HomeFooter';
-import Portfolio from './Homepage/Pages/Portfolio'; // Assuming this is the correct path
-import Contact from './Homepage/Pages/Contact'; // Assuming this is the correct path
+import Portfolio from './Homepage/Pages/Portfolio';
+import Contact from './Homepage/Pages/Contact';
 import './App.css';
 
 export default function MyApp() {
@@ -22,7 +22,6 @@ export default function MyApp() {
               <HomeFooter />
             </div>
           } />
-          {/* Use the element prop for Portfolio and Contact */}
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
